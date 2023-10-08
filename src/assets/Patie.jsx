69d27606b73c96912a8c8faa31f1654890346ea4
alpharagnarok <sonsurkarajitem@gmail.com>
2023-10-08 16:24:40 +0530

    updated

--- src/assets/Patie.jsx
+++ src/assets/Patie.jsx
@@ -1,17 +1,54 @@
-import { useState } from 'react'
+import PatientProfile from "../components/PatientProfile";
+import CustomizedTables from "../components/CustomizedTables";
+import "./Patie.css"
+import { useState } from 'react';
 
 function Patie() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const patientData = {
+    name: 'John Doe',
+    age: '33',
+    dateOfBirth: '1990-01-01',
+    bloodGroup: 'A+',
+    sex: 'Male',
+    Bmi: 25.0, // You can add BMI here if needed
+  };
+
+  const medicalHistory = [
+    {
+      day: 'Monday',
+      date: '2023-10-09',
+      doctor: 'Dr. Smith',
+      diagnosis: 'Fever',
+      prescription: 'Take rest and drink plenty of fluids',
+    },
+    {
+      day: 'Tuesday',
+      date: '2023-10-10',
+      doctor: 'Dr. Johnson',
+      diagnosis: 'Cough and Cold',
+      prescription: 'Take cough syrup and rest',
+    },
+    // Add more medical history entries here
+  ];
 
   return (
     <div>
-    <div className="patient-profile">
-    <h2>Patient Profile</h2>
-    <p>Name: patientData.name</p>
-    <p>Date of Birth: patientData.dateOfBirth</p>
-    <p>Blood Group: patientData.bloodGroup</p>
-    <p>Sex: patientData.sex</p>
-  </div>
-  </div>
-)
-  }export default Patie
+      {/* <div className="patient-profile">
+        <h2>Patient Profile</h2>
+        <p>Name: {patientData.name}</p>
+        <p>Age: {patientData.age}</p>
+        <p>Date of Birth: {patientData.dateOfBirth}</p>
+        <p>Blood Group: {patientData.bloodGroup}</p>
+        <p>Sex: {patientData.sex}</p>
+        <p>Bmi: {patientData.Bmi}</p> 
+      </div> */}
+      <PatientProfile></PatientProfile>
+<CustomizedTables></CustomizedTables>
+
+    </div>
+  );
+}
+
+export default Patie;
